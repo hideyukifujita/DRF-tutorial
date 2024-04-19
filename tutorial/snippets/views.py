74869669413497fef2ace6f7072e33b0be26c69e
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-19 14:56:31 +0900

    ✨ UserList, UserDetailクラスの追加

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -12,3 +12,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
