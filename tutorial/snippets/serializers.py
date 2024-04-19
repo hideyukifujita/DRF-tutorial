9d5d0ac699507f9df4bf325c90bdde53264a922a
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-19 14:54:36 +0900

    ✨  Userのシリアサイザーを追加

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
@@ -36,3 +37,10 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.style = validated_data.get("style", instance.style)
         instance.save()
         return instance
+    
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "snippets"]
