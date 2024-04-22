4b339e1d720d244c971788426d81a04fab332a59
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-22 17:28:50 +0900

    ✨ api_viewを追加し、ルートのエンドポイントを作成する

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
@@ -36,3 +39,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    """requestを受け取って、URLを含んだdictを返す
+
+    Args:
+        request (HttpRequest): HttpRequestオブジェクト
+        format (_type_, optional): _description_. Defaults to None.
+
+    Returns:
+        Response: URL
+    """
+    return Response({
+        "users": reverse("user-list", request=request, format=None),
+        "snippets": reverse("snippet-list", request=request, format=None),
+    })
