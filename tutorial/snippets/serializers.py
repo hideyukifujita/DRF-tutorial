3aec2576c4fb893f79ecd33839beb334ca101ff2
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-22 09:36:07 +0900

    ✨ serializerにownerフィールドを追加

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -5,39 +5,13 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style"]
+        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
 
-    def create(self, validated_data):
-        """validated_dataを受け取って、Snippetオブジェクトを返す
 
-        Args:
-            validated_data (dict): バリデーションデータ
-
-        Returns:
-            Snippet: Snippetオブジェクト
-        """
-        return Snippet.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        """instance, validatedを受け取って、既存のSnippetインスタンスを更新して返す
-
-        Args:
-            instance (Snippet): 既存のSnippetインスタンス
-            validated_data (dict): バリデーションデータ
-
-        Returns:
-            Snippet: Snippetインスタンス
-        """
-        instance.title = validated_data.get("title", instance.title)
-        instance.code = validated_data.get("code", instance.code)
-        instance.linenos = validated_data.get("linenos", instance.linenos)
-        instance.language = validated_data.get("language", instance.language)
-        instance.style = validated_data.get("style", instance.style)
-        instance.save()
-        return instance
-    
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
