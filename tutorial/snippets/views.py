9deaf5583363660241f74daf013855f7be2e82f3
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-22 09:42:03 +0900

    ✨ SnippetList, SnippetDetailにpermission_classesを追加

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,12 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         """リクエストのユーザー情報を保存する
@@ -20,6 +22,10 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+        ]
 
 
 class UserList(generics.ListAPIView):
